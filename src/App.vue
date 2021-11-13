a7eb0f193d7d8f2cfbcde4fb1e1bea6cfe214f4d
begoniasuccess <78841404+begoniasuccess@users.noreply.github.com>
2021-11-13 21:30:43 +0800

    axios test

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="assets/logo.png" />
+    <Test />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
+import Test from "./components/Test.vue";
 import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
   name: "App",
   components: {
     HelloWorld,
+    Test
   },
 };
 </script>
---
+++ src/components/Test.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="test">
+        <button class="btn btn-primary btn-lg btn-block" type="button" @click="test">Test</button>
+    </div>
+</template>
+
+<script>
+import Vue from "vue";
+export default {
+    data(){
+        return {
+
+        }
+    },
+    methods:{
+        test(){
+            let api = "https://618fb9d2f6bf450017484a23.mockapi.io/test";
+            Vue.axios.get(api).then((response) => {
+                console.log(response.data)
+            })
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .test{
+        width: 100%;;
+    }
+    button{
+        display: block;
+        width: 100%;
+    }
+</style>
