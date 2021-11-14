d7a4d8a32c02121c8b8494aebfb2214f62f18e92
begoniasuccess <78841404+begoniasuccess@users.noreply.github.com>
2021-11-14 13:26:08 +0800

    add vue-router

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,34 +1,38 @@
 <template>
-    <div class="test">
-        <button class="btn btn-primary btn-lg btn-block" type="button" @click="test">Test</button>
-    </div>
+  <div class="test">
+    <button
+      class="btn btn-primary btn-lg btn-block"
+      type="button"
+      @click="test"
+    >
+      Test
+    </button>
+  </div>
 </template>
 
 <script>
 import Vue from "vue";
 export default {
-    data(){
-        return {
-
-        }
+  data() {
+    return {};
+  },
+  methods: {
+    test() {
+      let api = "https://618fb9d2f6bf450017484a23.mockapi.io/test";
+      Vue.axios.get(api).then((response) => {
+        console.log(response.data);
+      });
     },
-    methods:{
-        test(){
-            let api = "https://618fb9d2f6bf450017484a23.mockapi.io/test";
-            Vue.axios.get(api).then((response) => {
-                console.log(response.data)
-            })
-        }
-    }
-}
+  },
+};
 </script>
 
 <style scoped>
-    .test{
-        width: 100%;;
-    }
-    button{
-        display: block;
-        width: 100%;
-    }
+.test {
+  width: 100%;
+}
+button {
+  display: block;
+  width: 100%;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,12 @@
 import Vue from "vue";
 import App from "./App.vue";
-import axios from 'axios';
-import VueAxios from 'vue-axios';
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
+import axios from "axios";
+import VueAxios from "vue-axios";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 // 載入 vue2-leaflet，依照自己需要載入組件
 import { LMap, LTileLayer, LMarker, LPopup, LIcon } from "vue2-leaflet";
-// 載入 css
 import "leaflet/dist/leaflet.css";
 
 Vue.use(VueAxios, axios);
@@ -19,17 +18,20 @@ Vue.component("l-tile-layer", LTileLayer);
 Vue.component("l-marker", LMarker);
 Vue.component("l-popup", LPopup);
 Vue.component("l-icon", LIcon);
+
 // 設定預設 icon
 import { Icon } from "leaflet";
+import router from "./router";
 delete Icon.Default.prototype._getIconUrl;
 Icon.Default.mergeOptions({
   iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
   iconUrl: require("leaflet/dist/images/marker-icon.png"),
-  shadowUrl: require("leaflet/dist/images/marker-shadow.png")
+  shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
 });
 
 Vue.config.productionTip = false;
 
 new Vue({
+  router,
   render: (h) => h(App),
 }).$mount("#app");
