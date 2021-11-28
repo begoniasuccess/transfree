3428a2a20b7577dd858d54da9cf360b491155494
juanchong <juanchong@hitrust.com.tw>
2021-11-28 23:03:05 +0800

    fix:blank list in mobile.

--- src/components/BlankList.vue
+++ src/components/BlankList.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="h_100 w_100">
-                  <div class="content_list">
-                    <div class="list_top flex_col_cc">搜尋清單</div>
-                    <div class="list_bottom flex_col_cc">
-                      <div class="img_box"></div>
-                    </div>
-                  </div>
+    <div class="content_list">
+      <div class="list_top flex_col_cc">搜尋清單</div>
+      <div class="list_bottom flex_col_cc">
+        <div class="img_box"></div>
+      </div>
+    </div>
   </div>
 </template>
 
